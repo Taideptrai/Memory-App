ccbf5ddee8d9cf7ccd7c3b2d61aa1380ace306f9
Taideptrai <nguyenvantaizzz@gmail.com>
2021-02-14 19:53:18 +0200

    make it better

---
+++ client/src/api/index.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+
+export const fetchPosts = () => axios.get('/posts/');
+export const createPost = (newPost) => axios.post('/posts', newPost);
+export const likePost = (id) => axios.patch(`/posts/${id}/likePost`);
+export const updatePost = (id, updatedPost) => axios.patch(`/posts/${id}`, updatedPost);
+export const deletePost = (id) => axios.delete(`/posts/${id}`);
---
+++ server/index.js
@@ -0,0 +1,30 @@
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+import path from 'path';
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/client/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/client/build/index.html'))
+);
+
+app.use('/posts', postRoutes);
+
+const CONNECTION_URL = 'mongodb+srv://TaiNguyen:tailacuachi123@cluster0.vnlzl.mongodb.net/momoryapp?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
